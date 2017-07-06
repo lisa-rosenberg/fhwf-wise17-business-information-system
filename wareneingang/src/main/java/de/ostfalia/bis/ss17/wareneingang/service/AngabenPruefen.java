950fa6359bb0a97f9e690899653f061aa5a8020d
Lisa Rosenberg <l.rosenberg@ostfalia.de>
2017-07-06 13:04:43 +0200

    Clean up code, Update html form files, Update bpmn file

--- wareneingang/src/main/java/de/ostfalia/bis/ss17/wareneingang/service/AngabenPruefen.java
+++ wareneingang/src/main/java/de/ostfalia/bis/ss17/wareneingang/service/AngabenPruefen.java
@@ -29,16 +29,10 @@ public class AngabenPruefen implements JavaDelegate {
         Integer teilId = (Integer) delegateExecution.getVariable("teilId");
         final Integer anderesTeilId = (Integer) delegateExecution.getVariable("anderesTeilId");
         final Integer mengeBestellt = (Integer) delegateExecution.getVariable("mengeBestellt");
-
         String teilBez = null;
-
-        Boolean teilExistiert = false;
-        Boolean lieferantExistiert = false;
-        Boolean angabenIo = false;
 
         /* Angaben prüfen */
 
-        // Nicht-Standard-Teil existiert?
         Class.forName("com.mysql.jdbc.Driver");
         final Connection conn = DriverManager.getConnection(
                 "jdbc:mysql://localhost/bis", "root", "mysql");
@@ -47,57 +41,45 @@ public class AngabenPruefen implements JavaDelegate {
         PreparedStatement stmt = conn.prepareStatement(
                 "SELECT COUNT(*) FROM teil WHERE TNR = ?");
 
+        // Existiert das angegebene Nicht-Standard-Teil?
         if (anderesTeilId != null) {
             stmt.setInt(1, anderesTeilId);
             ResultSet rs = stmt.executeQuery();
 
             if (rs.next() && rs.getInt(1) > 0) {
-                teilExistiert = true;
                 teilId = anderesTeilId;
+            } else {
+                throw new Exception("Teil existiert nicht in der Datenbank.");
             }
         }
 
-        // Standard-Teil existiert? (Korrekte Angabe eines anderen Teils ist stärker als Drop-Down-Auswahl)
-        if (!teilExistiert) {
-            stmt.setInt(1, teilId);
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next() && rs.getInt(1) > 0) {
-                teilExistiert = true;
-            }
-        }
-
-        if (teilExistiert) {
-            stmt = conn.prepareStatement(
-                    "SELECT BEZEICHNUNG FROM teil WHERE TNR = ?");
-
-            stmt.setInt(1, teilId);
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                teilBez = rs.getString(1);
-            }
-        }
-
-        // Lieferant existiert
+        // Hole Daten des Teils
         stmt = conn.prepareStatement(
-                "SELECT COUNT(*) FROM lieferant WHERE id_lieferant = ?");
+                "SELECT BEZEICHNUNG FROM teil WHERE TNR = ?");
 
-        stmt.setInt(1, lieferantId);
+        stmt.setInt(1, teilId);
         ResultSet rs = stmt.executeQuery();
 
-
-        if (rs.next() && rs.getInt(1) > 0) {
-            lieferantExistiert = true;
+        if (rs.next()) {
+            teilBez = rs.getString(1);
+        } else {
+            throw new Exception("Teil existiert nicht in der Datenbank.");
         }
 
-        // Angaben in Ordnung?
-        if (lieferantExistiert && teilExistiert) {
-            angabenIo = true;
+        // Existiert der angegebene Lieferant?
+        stmt = conn.prepareStatement(
+                "SELECT * FROM lieferant WHERE id_lieferant = ?");
+
+        stmt.setInt(1, lieferantId);
+        rs = stmt.executeQuery();
+
+        if (!rs.next()) {
+            throw new Exception("Lieferant existiert nicht in der Datenbank.");
         }
 
-        /* Bestellung speichern (für Demonstrationszwecke) */
+        /* Bestellung speichern (für Demonstrationszwecke, eigentlich wäre sie zu dem Zeitpunkt bereits vorhanden) */
 
+        // Neuen Beschaffungsauftrag anlegen
         stmt = conn.prepareStatement(
                 "SELECT MAX(id_auftrag) FROM beschaffungsauftrag");
         rs = stmt.executeQuery();
@@ -128,7 +110,6 @@ public class AngabenPruefen implements JavaDelegate {
 
         /* Gebe Ergebnisse weiter */
 
-        delegateExecution.setVariable("angabenIo", angabenIo);
         delegateExecution.setVariable("auftragId", auftragId);
         delegateExecution.setVariable("teilBez", teilBez);
     }
